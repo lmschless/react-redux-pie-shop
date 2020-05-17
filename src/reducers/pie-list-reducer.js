a29a4c3a17a1d7fcf560ddd425068b6956fc20cc
Luke <lmschless@gmail.com>
2020-05-16 23:57:18 -0700

    refactored pie list reducer to contain the pie list array and add it to intial state in the reducer.

--- src/reducers/pie-list-reducer.js
+++ src/reducers/pie-list-reducer.js
@@ -1,6 +1,46 @@
 import * as c from './../actions/ActionTypes';
+import { v4 } from 'uuid';
 
-export default (state = {}, action) => {
+const initialPieList = [
+	{
+		name: 'Pecan Pie',
+		longDescription:
+			'Pecan pie is a pie of pecan nuts mixed with a filling of eggs, butter, and sugar. ',
+		img: require('./../assets/pecan.jpeg'),
+		count: 8,
+		displayDetails: false
+	},
+	{
+		name: 'Apple Pie',
+		longDescription:
+			'Apple pie is an unofficial symbol of the United States and one of its signature comfort foods.',
+		img: require('./../assets/apple.jpeg'),
+		count: 5,
+		displayDetails: false
+	},
+	{
+		name: 'Cherry Pie',
+		longDescription:
+			'Pie baked with a cherry filling. Traditionally, cherry pie is made with tart rather than sweet cherries. ',
+		img: require('./../assets/cherry.jpg'),
+		count: 4,
+		displayDetails: false
+	},
+	{
+		name: 'Blueberry Pie',
+		longDescription:
+			'Pecan pie is a pie of pecan nuts mixed with a filling of eggs, butter, and sugar. ',
+		img: require('./../assets/blueberry.jpeg'),
+		count: 2,
+		displayDetails: false
+	}
+];
+
+this.state.pieList.forEach((pie) => {
+	pie.id = v4();
+});
+
+export default (state = initialPieList, action) => {
 	const { name, longDescription, img, count, displayDetails, id } = action;
 	switch (action.type) {
 		case c.PIE_LIST:
