7ad132c57296f8d6c41d0ac35f0691a1f32b7b11
Luke <lmschless@gmail.com>
2020-05-16 23:40:52 -0700

    adding redux and provider to index.js

--- src/__tests__/reducers/pie-list-reducer.test.js
+++ src/__tests__/reducers/pie-list-reducer.test.js
@@ -1,4 +1,20 @@
 import pieListReducer from '../../reducers/pie-list-reducer';
 import * as c from './../../actions/ActionTypes';
 
-let action;
+describe('pieListReducer', () => {
+	let action;
+
+	const currentState = {
+		1: {
+			name: 'Apple',
+			longDescription: 'This is a long description',
+			img: require('./../assets/apple.jpeg'),
+			count: 5,
+			displayDetails: false
+		}
+	};
+
+	test('Should return default state if no action type is recognized', () => {
+		expect(pieListReducer({}, { type: null })).toEqual({});
+	});
+});
